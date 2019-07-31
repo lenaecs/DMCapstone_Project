3a21e1f2557b4e0bc9f2b04a7de37fed9dfde3d4
lenaecs <lenae.stoner@gmail.com>
2019-07-31 18:47:58 -0500

    fml

--- capstone.py
+++ capstone.py
@@ -2,7 +2,7 @@ import os
 # import magic
 # import urllib.request
 from app import app
-from flask import Flask, flash, request, redirect, render_template
+from flask import flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = set(['csv', 'txt'])
@@ -12,12 +12,12 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/upload')
+@app.route('/')
 def upload_csv_lenae():
     return render_template('upload.html')
 
 
-@app.route('/upload', methods=['POST'])
+@app.route('//', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -32,7 +32,7 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             flash('File successfully uploaded')
-            return redirect('/upload')
+            return redirect('/')
         else:
             flash('Allowed file types are txt, csv')
             return redirect(request.url)
